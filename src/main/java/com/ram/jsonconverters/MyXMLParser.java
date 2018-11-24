963578e0fff24dcd1599d387812fb9e8e8df63e9
mittalas <mittalas@amazon.com>
2018-11-24 12:01:56 -0800

    Addding proto buff support and Jsockson samples

---
+++ src/main/java/com/ram/jsonconverters/MyXMLParser.java
@@ -0,0 +1,90 @@
+package com.ram.jsonconverters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyXMLParser {
+
+  public static void main(String arg[]) {
+
+    System.out.println("Staring ...");
+
+    MyXMLParser myXMLParser = new MyXMLParser();
+    myXMLParser.convertJavaToJSON();
+    myXMLParser.convertJavaToXML();
+    myXMLParser.convertXMLtoJava();
+
+    System.out.println("Conversion is done ....");
+  }
+
+
+  private void convertJavaToJSON() {
+    ObjectMapper mapper = new ObjectMapper();
+    Staff staff = getStaff();
+
+
+    try {
+      // Convert object to JSON string
+      String jsonInString = mapper.writeValueAsString(staff);
+      System.out.println(jsonInString);
+
+
+    } catch (JsonProcessingException ex) {
+      ex.printStackTrace();
+    }
+
+  }
+
+  private Staff getStaff() {
+
+    Map<String, Integer> wivesMap = new HashMap<>();
+    wivesMap.put("Gita", 1);
+    wivesMap.put("Pita", 2);
+
+    Staff staff = new Staff();
+    staff.setName("Ram");
+    staff.setAge(24);
+    staff.setSalary(new BigDecimal(123454L));
+    staff.setSkills(Arrays.asList("Java", "GO"));
+    staff.setPosition("Developer");
+
+
+    staff.setWivesCount(wivesMap);
+    return staff;
+  }
+
+  private void convertJavaToXML() {
+
+    try {
+
+      XmlMapper xmlMapper = new XmlMapper();
+      String stafXML = xmlMapper.writeValueAsString(getStaff());
+      System.out.println(stafXML);
+
+    } catch (Exception ex) {
+
+    }
+  }
+
+
+  private Staff convertXMLtoJava() {
+    Staff staff = null;
+    try {
+      File file = new File("/Volumes/Unix/workplace/PersonalProjects/docker-java-microservice-example/src/main/resources/StaffData.xml");
+      XmlMapper xmlMapper = new XmlMapper();
+      staff = xmlMapper.readValue(file, Staff.class);
+      System.out.println("done ... staff - " + staff.getName());
+    } catch (Exception ex) {
+
+      ex.printStackTrace();
+    }
+    return staff;
+  }
+}
---
+++ src/main/java/com/ram/jsonconverters/Staff.java
@@ -0,0 +1,71 @@
+package com.ram.jsonconverters;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@JsonPropertyOrder({"name", "wives", "age", "salary", "position", "skills"})
+@JsonRootName(value = "staff")
+public class Staff {
+
+  private String name;
+  private int age;
+  private String position;
+  private BigDecimal salary;
+  private List<String> skills;
+
+  private Map<String, Integer> wivesCount;
+
+  @JsonProperty("wives")
+  public Map<String, Integer> getWivesCount() {
+    return wivesCount;
+  }
+
+  public void setWivesCount(final Map<String, Integer> wives) {
+    this.wivesCount = wives;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(final int age) {
+    this.age = age;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public void setPosition(final String position) {
+    this.position = position;
+  }
+
+  public BigDecimal getSalary() {
+    return salary;
+  }
+
+  public void setSalary(final BigDecimal salary) {
+    this.salary = salary;
+  }
+
+  public List<String> getSkills() {
+    return skills;
+  }
+
+  public void setSkills(final List<String> skills) {
+    this.skills = skills;
+  }
+}
